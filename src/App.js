db732453a1d2819b3cea3d813a74fbc4b4bcb90a
Vitali_Rusau <Vitali_Rusau@epam.com>
2021-07-29 16:18:52 +0300

    Small update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="app-header app-background">
+                <h1 className="title">kaseopea</h1>
+                <p>Father 3x, dreamer, designer, software engineer at Epam.com</p>
+                <p>Hobbies: <a href="https://instagram.com/kaseopea/" target="_blank">photography</a> / <a
+                    href="https://www.instagram.com/kaseopea.design/" target="_blank">design</a></p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
